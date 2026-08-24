67f8a5a07f15fc61900e6fb397315f0ab2d93273
Eduardo Daniel Barrios Romero <eduardodanielbarriosromero891@gmail.com>
2026-08-24 10:33:26 -0600

    Add user credentials to Login class

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,6 @@
 public class Login{
     public static void main(String[] args) {
-        System.out.print("Hola Mundo!!")
+        String user = "Eddabar07";
+        String password = "070910";
     }
 }
